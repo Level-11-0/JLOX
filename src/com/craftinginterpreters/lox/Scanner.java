e6345c415c6cba14539d916ef399d7690001920e
level-11-0 <iamthepineapplegod@gmail.com>
2026-03-26 03:25:04 -0500

    Scanner Finished (Still have to add Recursive Commenting when I understand it)

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -1,11 +1,10 @@
 package com.craftinginterpreters.lox;
 
+import static com.craftinginterpreters.lox.TokenType.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static com.craftinginterpreters.lox.TokenType.*; 
 
 class Scanner{
 private final String source;
@@ -14,9 +13,31 @@ private int start = 0;
 private int current = 0;
 private int line = 1;
 
+
+private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
+    
 Scanner(String source){
     this.source = source;
-    }
+}
 
 List<Token> scanTokens(){
     while(!isAtEnd()){
@@ -49,7 +70,14 @@ private void scanToken(){
 
         case '/' -> {
             if(match('/')){
-                while(peak() != '\n' && !isAtEnd()) advance();
+                while(peek() != '\n' && !isAtEnd()) advance();
+            }else if(match('*')){
+                while(!(peek() == '*' && peekNext() == '/') && !isAtEnd()){
+                    if(peek() == '\n') line++;
+                    advance();
+                }
+                advance();
+                advance();
             } else {
             addToken(SLASH);
             }
@@ -61,14 +89,83 @@ private void scanToken(){
 
         case '\n' -> line++; //newline
 
-        default -> Lox.error(line, "Unexpected character.");
+        case '"' -> string();
+
+        default -> {
+            if (isDigit(c)) {
+                number();
+            } else if (isAlpha(c)) {
+                identifier();
+            } else {
+                Lox.error(line, "Unexpected character.");
+            }
+        }
     }
 }
 
-private char peak(){
+private void identifier(){
+    while(isAlphaNumeric(peek())) advance();
+    
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = IDENTIFIER;
+    addToken(type);
+}
+
+private void number(){
+    while(isDigit(peek())) advance();
+
+    if(peek() == '.' && isDigit(peekNext())){
+        // Consume the "."
+        advance();
+
+        while (isDigit(peek())) advance();
+    }
+
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+
+}
+
+private void string(){
+    while(peek() != '"' && !isAtEnd()){
+        if (peek() == '\n')line++;
+        advance();
+    }
+
+    if(isAtEnd()){
+        Lox.error(line, "Undetermined String.");
+        return;
+    }
+
+    advance();
+
+    String value = source.substring(start + 1, current -1);
+    addToken(STRING, value);
+}
+
+private char peek(){
     if (isAtEnd()) return '\0';
     return source.charAt(current);
 }
+
+private char peekNext(){
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+}
+
+private boolean isAlpha(char c){
+    return(c >= 'a' && c <='z') || 
+          (c >= 'A' && c <='Z') || 
+           c == '_';
+}
+
+private boolean isAlphaNumeric(char c){
+    return isAlpha(c) || isDigit(c);
+}
+
+private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+} 
 
 private boolean match(char expected){
     if (isAtEnd())return false;
@@ -77,6 +174,7 @@ private boolean match(char expected){
     current++;
     return true;
 }
+
 
 private char advance(){
     return source.charAt(current++);
@@ -95,4 +193,5 @@ private void addToken(TokenType type, Object literal){
 private boolean isAtEnd(){
     return current >= source.length();
 }
+
 }
